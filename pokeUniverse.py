6e578fc21bb9b46fded53f1fec2b02513a779836
zj4ck <omar.becci71@gmail.com>
2026-09-16 16:46:06 +0200

    API handling, no human readable

--- pokeUniverse.py
+++ pokeUniverse.py
@@ -1,8 +1,39 @@
 __author__ = "zj4ck3"
 
 # fetches Pokemon information from the API
+# return data of the pokemon if are available, None otherwise
 def pokemon_description(pokemon) -> dict:
-    pass
+    import requests
+
+    header = {"Content-Type":"application/json"}
+    URL = f"https://pokeapi.co/api/v2/pokemon/{pokemon}"
+
+    try:
+        response = requests.get(URL,headers=header,timeout=10)
+        response.raise_for_status()
+        pokeData = response.json() # create the dict
+
+    # Error handling
+    except requests.exceptions.Timeout:
+        print(f"\n[X] Timeout exceeded: {response.status_code}")
+        return
+
+    except requests.exceptions.ConnectionError:
+        print(f"\n[X] Unable to connect to server: {response.status_code}")
+        return
+
+    except requests.exceptions.HTTPError as error:
+        if response.status_code == 404:
+            print("\n[X] Pokemon not found")
+        else:
+            print(f"\n[X] HTTP Error: {error}")
+        return
+
+    except requests.exceptions.RequestException as error:
+        print(f"\n[X] Error during the request: {error}\n")
+        return
+
+    return pokeData
 
 # for option 5
 def give_information() -> None:
@@ -25,16 +56,26 @@ if __name__ == "__main__":
         
         try:
             option = int(input("[?] Choose an option: "))
+
             if option == 1:
-                pokemon = input("Name of the pokemon: ")
+                pokemon = input("[?] Name of the pokemon: ").capitalize()
+                pokeData = pokemon_description(pokemon) # the description must be readable
+
+                if pokeData != None:
+                    print(pokeData)
+
             elif option == 2:
                 print("2")
+
             elif option == 3:
                 print("3")
+
             elif option == 4:
                 print("4")
+
             elif option == 5:
                 give_information()
+
             else:
                 quit()
             
